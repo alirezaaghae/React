ddb58b7115c05e5460ea73ffe2534fd1d590fefe
Alireza Aghaei <appleiran66@gmail.com>
2023-03-19 15:50:26 +0330

    update 19Mar23

--- first-app/src/UserCard.js
+++ first-app/src/UserCard.js
@@ -4,23 +4,23 @@ import img1 from './image/img.png';
 const UserCard = (props) => {
     return(
         <div className="ui card">
-  <div className="image">
-    <img src={img1}/>
-  </div>
-  <div className="content">
-    <a className="header">Kristy</a>
-    <div className="meta">
-      <span className="date">Joined in 2013</span>
-    </div>
-    <div className="description">
-      {props.children}
-    </div>
-  </div>
-  <div className="ui bottom button">
-      <i className="add icon"></i>
-      Comment
-  </div>
-</div>
+          <div className="image">
+            <img src={img1}/>
+          </div>
+          <div className="content">
+            <a className="header">Kristy</a>
+            <div className="meta">
+              <span className="date">Joined in 2013</span>
+            </div>
+            <div className="description">
+              {props.children}
+            </div>
+          </div>
+          <div className="ui bottom button">
+              <i className="add icon"></i>
+              Comment
+          </div>
+        </div>
     )
 }
 
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -8,18 +8,13 @@ const App = () => {
     return(
         <UserCard>
         <div className='ui comments'>
-           
-            <SingleComment name='Alex' time='2' message='fuck you!'/>
-           
+            <SingleComment name='Alex' time='2' message='hallo!'/>
             <SingleComment name='Sarah' time='3' message='ja hallo' />
-            <SingleComment name='Neda' time='4' message='fick dich bitch' />
-            <SingleComment name='Nevin' time='5' message='Please help me mother fucker'/>
+            <SingleComment name='Neda' time='4' message='danke fur ihre app' />
+            <SingleComment name='Nevin' time='5' message='bitte hielf mir'/>
         </div>
         </UserCard>
     )
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />,document.querySelector('#root'))
